76270339fb1cb8da03058e2801a31dd6e9dca28c
listen936 <1697820682@qq.com>
2020-07-15 23:48:41 +0800

    feat: 浏览器知识1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,6 +20,15 @@ module.exports = {
         ],
         // sidebar:'auto',
         sidebar: [
+          {
+            title: '浏览器知识',   // 必要的
+          //   path: '/article/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 2,    // 可选的, 默认值是 1
+            children: [
+              ['/article/browser/render','浏览器的渲染过程']
+            ]
+          },
             {
               title: '其他',   // 必要的
             //   path: '/article/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
