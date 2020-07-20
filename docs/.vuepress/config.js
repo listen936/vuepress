792c0b9481965d0a801e531095b7bcb9ab875588
lswang <lswang@speehcx.cn>
2020-07-20 18:27:39 +0800

    feat: 浏览器存储-cookie

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -46,6 +46,7 @@ module.exports = {
         children: [
           ['/article/browser/render', '浏览器的渲染过程'],
           ['/article/browser/跨标签通信', '跨标签通信'],
+          ['/article/browser/存储', '浏览器存储'],
         ]
       },
       {
